3ed8295af234af8b896fb3d1968c2f9201d891b3
carlosDiaz <diaz-alzate@hotmail.com>
2023-03-16 22:13:35 -0500

    first commit

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use App\Models\User;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Register Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users as well as their
+    | validation and creation. By default this controller uses a trait to
+    | provide this functionality without requiring any additional code.
+    |
+    */
+
+    use RegistersUsers;
+
+    /**
+     * Where to redirect users after registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'image' => ['required'],
+            'identification' => ['required', 'integer', 'min:5454'],
+            'phoneNumber' => ['required', 'integer', 'min:5454'],
+
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return \App\Models\User
+     */
+    protected function create(array $data)
+    {
+        // $pass =  Hash::make($data['password']);
+        // dd($data);
+        // dd($data['image']->isFile('imagen')->store('public/images'));
+        // dd($data['image']->store('public/images'));
+        // dd($data['password']);
+        // if ($data['image']->hasFile('image')) {
+        // dd($data['image']);
+        $path = $data['image']->store('public/images');
+        dd($data['image'], $path, $data);
+        // }
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+            'image' => $path,
+            'identification' => $data['identification'],
+
+
+
+
+        ]);
+    }
+}
